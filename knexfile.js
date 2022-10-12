1cdca714030437ad6a0645b3f6a1a486d5c423a4
Dec <dec@Decagons-MacBook-Air.local>
2022-10-12 08:57:20 +0100

    chore: update knexfile with heroku details
    - update knexfile with heroku databse details from env

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,14 @@ require('dotenv').config()
 const DB_USERNAME = process.env.DATABASE_USER;
 const DB_PASSWORD = process.env.DATABASE_PASSWORD;
 const DB_NAME = process.env.DATABASE_NAME;
+const DB_HOST = process.env.DATABASE_HOST;
 
 module.exports = {
 
   development: {
     client: 'mysql2',
     connection: {
-      host: 'localhost',
+      host: DB_HOST,
       port: 3306,
       user: DB_USERNAME,
       password: DB_PASSWORD,
